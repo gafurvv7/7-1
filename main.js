a0f6bc3941833b5895ebecf195ba7912dc5d81a3
gafurvv7 <168542427+gafurvv7@users.noreply.github.com>
2024-08-13 15:13:20 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let ism = prompt("Ismingizni kiriting!");
+let familiya = prompt("Familiyangzni Kiriting!");
+let yosh = prompt("Yoshingzni kiriting");
+let xobbi = prompt("Yoqtirgan xobbiyingiz");
+let joyi = prompt("Turar joyingizni kiriting");
+
+console.log(ism);
+console.log(familiya);
+console.log(yosh);
+console.log(xobbi);
+console.log(joyi);
